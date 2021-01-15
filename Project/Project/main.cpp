b85d8a9a98a1612f20e67572f5d57390bc8b7d76
Daniel Mata <danielmata836@gmail.com>
2021-01-15 17:53:57 +0000

    DeVez

---
+++ Project/Project/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+#include "Header.h"
+#include "Header1.h"
+
+
+int main()
+{
+	// Usar o ficheiro do header
+
+	Jogo game;
+	game.ConstructConsole(160, 100, 8, 8);
+	game.Start();
+	return 0;
+}
